ca1f43e69d80fc83d0f896a8dc7afe8695edad1a
Matheus Muccio <matheus.muccio@rodojacto.com.br>
2024-01-18 10:10:35 -0300

    feat: concludeOs method

--- mobile/src/app/shared/services/os.service.ts
+++ mobile/src/app/shared/services/os.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { OrderService } from '../models/os.model';
+import { ConcludeOS, ConcludeOsResponse, OrderService } from '../models/os.model';
 
 @Injectable({
   providedIn: 'root'
@@ -17,5 +17,9 @@ export class OsService {
 
   getOrderServices(id: number): Observable<{error: string, result: OrderService[]}>{
     return this.http.get<{error: string, result: OrderService[]}>(`${this.API}/ordem-servico/tecnico/${id}`);
+  }
+
+  concludeOs(values: ConcludeOS): Observable<ConcludeOsResponse> {
+    return this.http.patch<ConcludeOsResponse>(`${this.API}/ordem-servico/concluir`, values);
   }
 }
