6ecb7e3ddbf111ab84041fc1822ad790a6dd6c3b
Matheus Muccio <matheus.muccio@rodojacto.com.br>
2024-01-25 11:21:46 -0300

    feat: adding auth guard

--- mobile/src/app/app-routing.module.ts
+++ mobile/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './shared/guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -13,6 +14,7 @@ const routes: Routes = [
   },
   {
     path: 'tabs',
+    canActivate: [AuthGuard],
     loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
   }
 ];
---
+++ mobile/src/app/shared/guards/auth.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable, from, of, switchMap } from 'rxjs';
+import { StorageService } from '../services/storage.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private storage: StorageService,
+    private router: Router
+  ) { }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    return from(this.storage.get('token')).pipe(
+      switchMap(token => {
+        console.log('AuthToken: ', token);
+
+        if (!token || token === undefined) {
+          return this.router.navigateByUrl('/auth').then(() => false);
+        } else {
+          return of(true);
+        }
+      })
+    );
+  }
+}
